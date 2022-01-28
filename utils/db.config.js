0aea6970a338de43f473ccf0f182c7dd95b0ff01
Cong Phan <ptcongbk@gmail.com>
2022-01-29 02:16:25 +1000

    Change DB configuration

--- utils/db.config.js
+++ utils/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "ec2-34-236-87-247.compute-1.amazonaws.com",
-    USER: "shflbadutcjcan",
-    PASSWORD: "25982259431e16c3375898cae93a8a87a5c20298546e81267f7c90d6eadd1d55",
-    DB: "dcnos9claboqaj",
+    HOST: "ec2-18-208-102-44.compute-1.amazonaws.com",
+    USER: "mhejkbjvamszsk",
+    PASSWORD: "f69cf216fdb7feccee5aacd71813d3d04f621261a7ea18eeba546703d86b5ecc",
+    DB: "d6r4ggie9re1re",
     port: 5432,
     dialect: "postgres",
     pool: {
